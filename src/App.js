e2a704ec75799ddba8e8054a19171d208390f85a
Aayush <aayushrathi237@gmail.com>
2024-01-01 16:09:52 +0530

    CRA and Tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-3xl font-bold">
+      Hello everyone lets build netflix gpt.
     </div>
   );
 }
 
 export default App;
+
+/*
+Login/Signup
+  - Sign in/Sign up form
+  - Redirect to browse page
+
+Browse (after authentication)
+  - Header
+  - Main Movie
+    - Trailer in Background
+    - Title and Description
+    - Movie Suggestions
+      - Movie Lists * N
+    
+Netflix GPT
+  - Search Bar
+  - Movie Suggestions
+*/
